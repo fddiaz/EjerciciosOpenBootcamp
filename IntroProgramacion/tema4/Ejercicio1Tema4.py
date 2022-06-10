a1312c14a7800ff1c3afaf00e8fed2928f33d858
Federico Diaz <fddiaz@gmail.com>
2022-06-10 20:33:56 -0300

    Correcciones varias

--- IntroProgramacion/tema4/Ejercicio1Tema4.py
+++ IntroProgramacion/tema4/Ejercicio1Tema4.py
@@ -1,8 +1,10 @@
 #@author Fedrico Damaso Diaz Ramseyer
 
-numeroIf = -1
+numeroIf = 4
 
 if (numeroIf > 0):
     print("Número positivo.")
+elif (numeroIf == 0):
+    print("Cero")
 else:
     print("Número negatico.")
