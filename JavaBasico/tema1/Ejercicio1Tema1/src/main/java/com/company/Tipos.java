4af167b274145d41952a332a283941173dd97628
Federico Diaz <fddiaz@gmail.com>
2022-07-31 15:14:58 -0300

    Java Básico Ejercicio 1 Tema 1

---
+++ JavaBasico/tema1/Ejercicio1Tema1/src/main/java/com/company/Tipos.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Tipos {
+    public static void main(String[] args) {
+        System.out.println("Tipos de datos entetos");
+
+        byte vByte = 8;
+        System.out.println("vByte = " + vByte);
+
+        short vShort = 32767;
+        System.out.println("vShort = " + vShort);
+
+        int vInt = 22;
+        System.out.println("vInt = " + vInt);
+
+        long vLong = 345543455;
+        System.out.println("vLong = " + vLong);
+
+        System.out.println("\nTipos de datos decimal");
+
+        float vFloat = 32f;
+        System.out.println("vFloat = " + vFloat);
+
+        double vDouble = 64.3d;
+        System.out.println("vDouble = " + vDouble);
+
+        System.out.println("\nTipos de datos boolean");
+
+        boolean vBooleanTrue = true;
+        System.out.println("vBooleanTrue = " + vBooleanTrue);
+        boolean vBooleanFalse = false;
+        System.out.println("vBooleanFalse = " + vBooleanFalse);
+
+        System.out.println("\nTipos de dato String");
+
+        char vChar = 'g';
+        System.out.println("vChar = " + vChar);
+
+        String vString = "Hola mundo!";
+        System.out.println("vString = " + vString);
+    }
+}
