70f00bc3ea3b037912554b3a2196f366d193c270
Federico Diaz <fddiaz@gmail.com>
2022-09-20 16:35:38 -0300

    JavaScript Básico Ejercicio 1 Tema 1 (to list)

--- JavaScriptBasico/tema2/ejercicio.js
+++ JavaScriptBasico/tema2/ejercicio.js
@@ -1,19 +1,14 @@
-const name  = "Federico Diaz";
-console.log(name);
+const list = [
+    "Federico Diaz",
+    42,
+    true,
+    new Date("08/22/1980"),
+    {
+        title: "El arte de amar",
+        author: "Erik From",
+        date: 1956,
+        url: "https://es.wikipedia.org/wiki/El_arte_de_amar_(Fromm)"
+    }
+];
 
-const age = 42;
-console.log(age);
-
-const developer = true;
-console.log(developer);
-
-const birthday = new Date("08/22/1980");
-console.log(birthday);
-
-const favoriteBook = {
-    title: "El arte de amar",
-    author: "Erik From",
-    date: 1956,
-    url: "https://es.wikipedia.org/wiki/El_arte_de_amar_(Fromm)"
-}
-console.log(favoriteBook);
+console.log(list);
