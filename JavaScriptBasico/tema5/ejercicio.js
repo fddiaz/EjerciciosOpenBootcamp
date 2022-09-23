c13e5c941427147d9140ccc21db86ea281e565bd
Federico Diaz <fddiaz@gmail.com>
2022-09-22 22:26:38 -0300

    JavaScript Básico Ejercicio 1 Tema 5

---
+++ JavaScriptBasico/tema5/ejercicio.js
@@ -0,0 +1,9 @@
+let heightCm = 182;
+let heightM = 1.82;
+
+let weight =82.8;
+
+let heightRoundedUp = Math.ceil(heightM)
+let weightRoundedKg = Math.floor(weight);
+
+let areEqual = Number.MAX_VALUE + 1 === Number.MAX_VALUE
