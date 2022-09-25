aa59882e7741072c91e2a250f87168cbf6713a7c
Federico Diaz <fddiaz@gmail.com>
2022-09-25 16:44:55 -0300

    JavaScript Básico Ejercicio 1 Tema 7

---
+++ JavaScriptBasico/tema7/conjuntos.js
@@ -0,0 +1,8 @@
+const familia = new Set(["Federico", "Carolina", "Navia", "Deva"])
+console.log(familia);
+
+familia.add("Federico")
+console.log(familia)
+
+familia.add("Javascript")
+console.log(familia)
