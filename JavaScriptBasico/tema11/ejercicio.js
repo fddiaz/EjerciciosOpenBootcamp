21a4cfddb4d228ae60b413a529611d580158107f
Federico Diaz <fddiaz@gmail.com>
2023-01-06 20:59:20 -0300

    JavaScript Básico Ejercicio 1 Tema 11

---
+++ JavaScriptBasico/tema11/ejercicio.js
@@ -0,0 +1,15 @@
+class Estudiante {
+    nombre = "Federico";
+    asignaturas = ["JavaScript", "HTML", "CSS"];
+
+    obtenDatos() {
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas;
+        }
+    }
+}
+
+const nuevoEstudiante = new Estudiante();
+
+console.log(nuevoEstudiante.obtenDatos());
